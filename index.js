48ff2bae2dae2e4b6c69a7542f43fe685433a189
Samuel Baraka <barakasamuel125@gmail.com>
2025-06-26 22:09:44 +0300

    Updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+if (main) main.remove();
+
+window.newHeader = document.createElement('h1'); 
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'BARAKA is the champion'; 
+
+
+document.body.appendChild(newHeader);
